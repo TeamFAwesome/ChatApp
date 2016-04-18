e29dba1a9e909ba2078e914138797d0824047ebd
nuclearmistake <nuclearmistake@gmail.com>
2016-04-18 12:38:51 -0400

    Theoretically wrap the python API wrapper with websockets to json

--- EIESWrapper.js
+++ EIESWrapper.js
@@ -8,7 +8,7 @@ api.onclose = function (e) {
         element["callback"](element);
     }
     pendingcalls=[]
-    api = new WebSocket("ws://localhost:10000/chat");
+    api = new WebSocket("ws://localhost:11000/api");
 };
 
 api.onmessage = function (e) {
@@ -37,14 +37,13 @@ api.onmessage = function (e) {
     }
     if (returnedcall != null)
     {
-        $scope.$apply(function () {
-            pendingcall["callback"](returnedcall["result"]);
-        })
-        pendingcalls.splice(array.indexOf(returnedcall), 1)
+        returnedcall["callback"](data["result"]);
+        pendingcalls.splice(array.indexOf(returnedcall), 1);
     }
     else
     {
         console.log("NOT SURE WHICH PENDING CALL ACTUALLY RETURNED A MESSAGE FROM PYTHON LAND");
+        console.log(data);
     }
 };
 
@@ -58,4 +57,136 @@ var invoke = function (callback, funcname, args) {
     ws.send(JSON.stringify(pendingcall["args"]));
 }
 
+//// BEGIN SESSION
+function Login(email, password)
+{
+  var r;
+  invoke(function(result){ r = result; }, "Login", {email: email, password: password});
+  while (!r) {}
+  return r;
+}
+function Logout()
+{
+  var r;
+  invoke(function(result){ r = result; }, "Logout", {});
+  while (!r) {}
+  return r;
+}
+    
+//// END SESSION
+    
+    
+//// BEGIN USER INFO STUFF
+function GetUserInfo()
+{
+  var r;
+  invoke(function(result){ r = result; }, "GetUserInfo", {});
+  while (!r) {}
+  return r;
+}
+//// BEGIN USER INFO STUFF
 
+
+//// BEGIN KEY STUFF
+function LookupPubKey(domain, port)
+{
+  var r;
+  invoke(function(result){ r = result; }, "LookupPubKey", {domain: domain, port: port});
+  while (!r) {}
+  return r;
+}
+
+function NewKey(name, body)
+{
+  var r;
+  invoke(function(result){ r = result; }, "NewKey", {name: name, body: body});
+  while (!r) {}
+  return r;
+}
+
+function RetrieveKey(key_id)
+{
+  var r;
+  invoke(function(result){ r = result; }, "RetrieveKey", {key_id: key_id});
+  while (!r) {}
+  return r;
+}
+
+function UpdateKey(name, body)
+{
+  var r;
+  invoke(function(result){ r = result; }, "UpdateKey", {name: name, body: body});
+  while (!r) {}
+  return r;
+}
+
+function DestroyKey(key_id)
+{
+  var r;
+  invoke(function(result){ r = result; }, "DestroyKey", {key_id});
+  while (!r) {}
+  return r;
+}
+//// END KEY STUFF
+
+
+//// BEGIN ENTITY STUFF
+function NewEntity(name, domain, port)
+{
+  var r;
+  invoke(function(result){ r = result; }, "NewEntity", {name: name, domain: domain, port: port});
+  while (!r) {}
+  return r;
+}
+
+function RetrieveEntity(entity_id)
+{
+  var r;
+  invoke(function(result){ r = result; }, "RetrieveEntity", {entity_id: entity_id});
+  while (!r) {}
+  return r;
+}
+
+function UpdateEntity(name, domain, port)
+{
+  var r;
+  invoke(function(result){ r = result; }, "UpdateEntity", {name: name, domain: domain, port: port});
+  while (!r) {}
+  return r;
+}
+
+function DestroyEntity(entity_id)
+{
+  var r;
+  invoke(function(result){ r = result; }, "DestroyEntity", {entity_id: entity_id});
+  while (!r) {}
+  return r;
+}
+//// END ENTITY STUFF
+
+
+//// BEGIN ENTITY TOKEN STUFF
+function CreateEntityToken(entity_id, key_id)
+{
+  var r;
+  invoke(function(result){ r = result; }, "CreateEntityToken", {entity_id: entity_id, key_id: key_id});
+  while (!r) {}
+  return r;
+}
+
+function RetrieveEntityToken(token_id, session_id)
+{
+  var r;
+  invoke(function(result){ r = result; }, "RetrieveEntity", {token_id: token_id, session_id: session_id});
+  while (!r) {}
+  return r;
+}
+
+function DestroyEntityToken(token_id, session_id)
+{
+  var r;
+  invoke(function(result){ r = result; }, "DestroyEntity", {token_id: token_id, session_id: session_id});
+  while (!r) {}
+  return r;
+}
+//// END ENTITY TOKEN STUFF
