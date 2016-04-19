1c4c676713fc65bcb5876f31f21d4d0daf898d81
mikemeding <mikeymeding@gmail.com>
2016-04-19 14:53:40 -0400

    added login panel and are now using that as the username

--- ChatApp.js
+++ ChatApp.js
@@ -5,9 +5,9 @@ var app = angular.module('ChatApp', []);
 
 app.controller("Main", function ($scope, $http) {
     $scope.title = "EIES WebSocket Demo";
-    $scope.author = "";
     $scope.text = "";
     $scope.messages = [];
+    $scope.username = "";
 
     //var ws = WebsocketService.open();
     var ws = new WebSocket("ws://localhost:10000/chat");
@@ -28,17 +28,31 @@ app.controller("Main", function ($scope, $http) {
     };
 
     $scope.send = function () {
-        //WebsocketService.sendMessage($scope.author, $scope.text);
+        // format message
         var data = {
-            author: $scope.author,
+            author: $scope.username,
             message: $scope.text
         };
         console.log("sending message");
         console.log(data);
+
+        //ENCRYPT AND GET SHIT DONE HERE
+
+        // this sends message to websocket
         ws.send(JSON.stringify(data));
 
         // reset scope
         $scope.text = "";
+    };
+
+    $scope.loggedIn = false;
+    $scope.login = function (username, password) {
+        console.log(username + "," + password);
+        $scope.loggedIn = true; // when this variable changes the view changes automatically. ohhh. magic.
+
+        // THIS IS WHERE YOU CAN ADD SHIT FOR AFTER LOGIN.
+        //$scope.username gets tracked and is used for messages now.
+
     };
 
 });
