1becd2c3ee047a1f10ccdc3dee989ad337f8445a
nuclearmistake <nuclearmistake@gmail.com>
2016-04-19 16:58:48 -0400

    ChatServer: print when changing buddy status

--- ChatServer.py
+++ ChatServer.py
@@ -36,10 +36,12 @@ class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
             print("Warning: received unknown message from client: %s" % data)
 
     def send_buddy_online(self):
+        print("Sending online status of %s" % self.name)
         for c in clients:
             clients[c].write_message(json.dumps({"type": "buddy_online", "name": self.name}))
 
     def send_buddy_offline(self):
+        print("Sending offline status of %s" % self.name)
         for c in clients:
             clients[c].write_message(json.dumps({"type": "buddy_offline", "name": self.name}))
 
