6dc4eb8982b4fb79aafb88beae3d56c815dec2e1
nuclearmistake <nuclearmistake@gmail.com>
2016-04-19 15:25:57 -0400

    Make clients aware of what other clients are online or offline for per-message crypto

--- ChatApp.js
+++ ChatApp.js
@@ -2,6 +2,8 @@
  * Created by mike on 4/17/16.
  */
 var app = angular.module('ChatApp', []);
+var buddies = [];
+var my_name;
 
 app.controller("Main", function ($scope, $http) {
     $scope.title = "EIES WebSocket Demo";
@@ -11,31 +13,60 @@ app.controller("Main", function ($scope, $http) {
 
     //var ws = WebsocketService.open();
     var ws = new WebSocket("ws://localhost:10000/chat");
+    ws.onopen = function() {
+        if (my_name)
+            ws.send(JSON.stringify({type: "hello", name: my_name}));
+    }
 
     // register onclose so that it will constantly retry
     ws.onclose = function (e) {
         console.error("connection lost. reconnecting.");
         ws = new WebSocket("ws://localhost:10000/chat");
+        ws.onopen = function() {
+            if (my_name)
+                ws.send(JSON.stringify({type: "hello", name: my_name}));
+        }
     };
 
     ws.onmessage = function (e) {
         var data = JSON.parse(e.data);
         console.log("got message");
         console.log(data);
-        $scope.$apply(function () {
-            $scope.messages.push(data); // add our message to our backlog
-        })
+        switch (data.type)
+        {
+            case 'msg':
+                //decrypt here
+                $scope.$apply(function () {
+                    $scope.messages.push({author: data.author, message: data.message}); // add our message to our backlog
+                });
+                break;
+            case 'buddy_online':
+                console.log("buddy: " + data.name + "is online");
+                buddies.push(data.name);
+                break;
+            case 'buddy_offline':
+                console.log("buddy: " + data.name + "is offline");
+                buddies.splice(buddies.index_of(data.name),1);
+                break
+        }
     };
 
     $scope.send = function () {
-        //WebsocketService.sendMessage($scope.author, $scope.text);
         var data = {
             author: $scope.author,
             message: $scope.text
         };
-        console.log("sending message");
-        console.log(data);
-        ws.send(JSON.stringify(data));
+        console.log("sending message: "+data);
+        for(var buddy in buddies)
+        {
+            var message = {
+                type: "msg",
+                author: data.author,
+                message: data.message,
+                destination: buddy
+            }
+            ws.send(JSON.stringify(data));
+        }
 
         // reset scope
         $scope.text = "";
--- ChatServer.py
+++ ChatServer.py
@@ -2,8 +2,10 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import inspect
+import json
 
-clients = []
+clients = {}
+namelessclients=[]
 
 class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
@@ -11,15 +13,48 @@ class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
 
     def open(self, *args):
         print("open", "WebSocketChatHandler")
-        clients.append(self)
+        namelessclients.append(self)
+        print("#nameless = %d" % len(namelessclients))
+        self.name = None
 
     def on_message(self, message):
-        print(message)
-        for client in clients:
-            client.write_message(message)
+        data = json.loads(message)
+        if data["type"] == "hello":
+            print("HELLO %s ! %d nameless" % (self.name, len(namelessclients)))
+            self.name = data["name"]
+            if self in namelessclients:
+                namelessclients.remove(self)
+            if not self.name in clients:
+                clients[self.name] = self
+                self.send_buddy_online()
+        elif data["type"] == "msg":
+            if not data["destination"] in clients.keys():
+                print("Error: unknown message destination\n\t%s" % data)
+            else:
+                clients[data["destination"]].write_message(message)
+        else:
+            print("Warning: received unknown message from client: %s" % data)
+
+    def send_buddy_online(self):
+        for c in clients:
+            clients[c].write_message(json.dumps({"msg": "buddy_online", "name": self.name}))
+
+    def send_buddy_offline(self):
+        for c in clients:
+            clients[c].write_message(json.dumps({"msg": "buddy_offline", "name": self.name}))
 
     def on_close(self):
-        clients.remove(self)
+        if self in namelessclients:
+            namelessclients.remove(self)
+        else:
+            del clients[self.name]
+            self.send_buddy_offline()
+
+    def __str__(self):
+        if self.name and self.name != None:
+            return self.name
+        return "[UNKNOWN]"
+
 
 app = tornado.web.Application([(r'/chat', WebSocketChatHandler)])
 app.listen(10000)
